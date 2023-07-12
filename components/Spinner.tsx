6a519408af90f8bc5ef219a363fba3c08b9f9d34
BorislavBranimirov <boriintorr@gmail.com>
2023-07-12 01:58:11 +0100

    added loading spinner animation

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,11 +1,49 @@
-import { StyleSheet, View } from 'react-native';
+import { Animated, Easing, StyleSheet, View } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
+import { useEffect, useRef } from 'react';
+
+const AnimatedViewIcon = Animated.createAnimatedComponent(FontAwesome);
 
 const Spinner = () => {
+  const spinValue = useRef(new Animated.Value(0)).current;
+
+  // Looping stops after one interation with useNativeDriver set
+  // Wrapping .timing in .parallel fixes it
+  useEffect(() => {
+    Animated.loop(
+      Animated.parallel([
+        Animated.timing(spinValue, {
+          toValue: 1,
+          duration: 2000,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+
+    return () => {
+      spinValue.stopAnimation();
+    };
+  }, [spinValue]);
+
   return (
     <View style={styles.spinnerWrapper}>
-      <FontAwesome name="spinner" size={48} color={Colors.accent} />
+      <AnimatedViewIcon
+        name="spinner"
+        size={48}
+        color={Colors.accent}
+        style={{
+          transform: [
+            {
+              rotate: spinValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0deg', '360deg'],
+              }),
+            },
+          ],
+        }}
+      />
     </View>
   );
 };
