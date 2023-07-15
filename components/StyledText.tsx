1a479dcaa0bc12417abc06fd42c437d90d3fb1ff
BorislavBranimirov <boriintorr@gmail.com>
2023-07-15 12:11:04 +0100

    properly change custom font to bold

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,14 +1,22 @@
-import { Text, TextProps } from 'react-native';
+import { StyleSheet, Text, TextProps } from 'react-native';
 import Colors from '../constants/Colors';
 
 const InterText = (props: TextProps) => {
+  // Properly switch between Inter regular and Inter bold, since React Native
+  // does not do that automatically (no font-face rule)
+  const fontWeight = StyleSheet.flatten(props.style).fontWeight;
+  const isBold = fontWeight === 'bold' || fontWeight === '700';
+
   return (
     <Text
       {...props}
       style={[
         { color: Colors.text, fontSize: 16, lineHeight: 24 },
         props.style,
-        { fontFamily: 'Inter_400Regular' },
+        {
+          fontFamily: isBold ? 'Inter_700Bold' : 'Inter_400Regular',
+          fontWeight: '400',
+        },
       ]}
     />
   );
