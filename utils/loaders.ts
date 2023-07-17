af75ab1331b38d3b94158341042ad9f33241ebd8
BorislavBranimirov <boriintorr@gmail.com>
2023-07-17 14:06:25 +0100

    jikan api now sorts by relevancy and properly handles multi-word/related-word searches

--- utils/loaders.ts
+++ utils/loaders.ts
@@ -30,10 +30,10 @@ export const fetchSearchAnime = async (
   const url = new URL('https://api.jikan.moe/v4/anime');
   url.searchParams.append('limit', '20');
   url.searchParams.append('sfw', 'true');
-  url.searchParams.append('order_by', 'mal_id');
-  url.searchParams.append('sort', 'asc');
+  // url.searchParams.append('order_by', 'mal_id');
+  // url.searchParams.append('sort', 'asc');
   if (query) {
-    url.searchParams.append('q', `"${query}"`);
+    url.searchParams.append('q', query);
   }
   if (page) {
     url.searchParams.append('page', page);
